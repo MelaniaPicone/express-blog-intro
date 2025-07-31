65e233e3c76d2cceaca16a059fc638999c6e5ddf
MelaniaPicone <melania_picone@yahoo.it>
2025-07-31 21:05:03 +0200

    [add] img

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+
+// importo express
 const express = require('express');
+
+// definisco app
 const app = express ();
+
+// definisco porta dove deve rimanere in ascolto il server
 const port = 3000;
 
-app.get('/', (req, res) => {});
-app.listen(port, () => {'Server del mio blog'});
+// middleware per i file statici
+app.use(express.static('public'));
+
+// definisco la rotta base
+app.get('/', (req, res) => {res.send('Il mio Blog')});
+
+// dico al server di rimanere in ascolto
+app.listen(port, () => { console.log(`Server del mio blog sulla porta ${port}`) });
