1d3650ca402ab69d6e61964d568716112bd05141
MelaniaPicone <melania_picone@yahoo.it>
2025-07-31 21:27:10 +0200

    [add] array

--- app.js
+++ app.js
@@ -14,5 +14,50 @@ app.use(express.static('public'));
 // definisco la rotta base
 app.get('/', (req, res) => {res.send('Il mio Blog')});
 
+// definisco la rotta bacheca
+app.get('/bacheca', (req, res) => {
+const bacheca = [
+{
+  name: "Espeon",
+  content: "Psycho",
+  image: "img/espeon.jpg",
+  tags: ["pokemon", "psycho", "cute", "purple"],
+}, 
+
+{
+  name: "Glaceon",
+  content: "Ice",
+  image: "img/glaceon.jpg",
+  tags: ["pokemon", "psycho", "cute", "purple"],
+},
+
+{
+  name: "Leafeon",
+  content: "Grass",
+  image: "img/leafeon.jpg",
+  tags: ["pokemon", "psycho", "cute", "purple"],
+},
+
+{
+  name: "Sylveon",
+  content: "Poison",
+  image: "img/sylveon.jpg",
+  tags: ["pokemon", "psycho", "cute", "purple"],
+},
+
+{
+  name: "Umbreon",
+  content: "Dark",
+  image: "img/umbreon.jpg",
+  tags: ["pokemon", "psycho", "cute", "purple"],
+}
+];
+
+res.json(bacheca);
+
+});
+
+
 // dico al server di rimanere in ascolto
-app.listen(port, () => { console.log(`Server del mio blog sulla porta ${port}`) });
+app.listen(port, () => { console.log(`Server del mio blog sulla porta ${port}`) }); 
+
